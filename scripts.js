27a952651ff246a77355c2a9ef94d1efc132b1e9
neel1303 <155171758+neel1303@users.noreply.github.com>
2024-01-02 21:34:38 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('askButton').addEventListener('click', function () {
+    alert('Sending all my love to you! I love you the most. 💖');
+    // You can add additional actions or animations here
+});
